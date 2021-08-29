8a69c3d50c258980172bec2cf5d2b072e1cfc8a9
vichekaphkaymedia <vicheka.phkaymedia@gmail.com>
2021-08-29 13:11:20 +0700

    Download excel works

--- controllers/user.js
+++ controllers/user.js
@@ -2,29 +2,63 @@ const ErrorResponse = require('../utils/errorResponse');
 const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 const validateUser = require('../validation/UserValidation');
-const Joi = require('joi');
+const excel = require("exceljs")
 
 const index = asyncHandler(async (req, res) => {
     const users = await User.find();
     res.status(200).json({
-      success: true,
-      data: users
+        success: true,
+        data: users
     });
 });
 
+const downloadExcel = asyncHandler(async (req, res, next) => {
+    const workbook = new excel.Workbook()
+    const worksheet = workbook.addWorksheet('users')
+    let users = await User.find().lean();
+    users = users.map((user, index) => {
+        user.id = ++index
+        return user
+    })
+
+    worksheet.columns = [
+        {header: "ID", key: "id", width: 5},
+        {header: "Username", key: "username", width: 20},
+        {header: "First Name", key: "firstName", width: 20},
+        {header: "Last Name", key: "lastName", width: 20},
+        {header: "Email", key: "email", width: 30},
+        {header: "Password", key: "password", width: 20},
+    ]
+
+    worksheet.addRows(users)
+
+    res.setHeader(
+        "Content-Type",
+        "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+    );
+    res.setHeader(
+        "Content-Disposition",
+        "attachment; filename=" + "users.xlsx"
+    );
+
+    return workbook.xlsx.write(res).then(function () {
+        res.status(200).end();
+    });
+})
+
 const show = asyncHandler(async (req, res, next) => {
-  const user = await User.findById(req.params.id);
-  res.status(200).json({
-    success: true,
-    data: user
-  });
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+        success: true,
+        data: user
+    });
 });
 
 
 const create = asyncHandler(async (req, res, next) => {
     const {error} = await validateUser(req.body);
     // res.send(validation)
-    if(error){
+    if (error) {
         return res.status(400).send(error.details);
     }
     user = await User.create(req.body);
@@ -35,24 +69,24 @@ const create = asyncHandler(async (req, res, next) => {
 });
 
 const update = asyncHandler(async (req, res, next) => {
-  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-    new: true,
-    runValidators: true
-  });
-  res.status(200).json({
-    success: true,
-    data: user
-  });
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true
+    });
+    res.status(200).json({
+        success: true,
+        data: user
+    });
 });
 
 
 const destroy = asyncHandler(async (req, res, next) => {
-  await User.findByIdAndDelete(req.params.id);
-  res.status(200).json({
-    success: true,
-    data: {}
-  });
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+        success: true,
+        data: {}
+    });
 });
 
 
-module.exports = {index,show,create,update,destroy}
+module.exports = {index, show, create, update, destroy, downloadExcel}
--- routes/users.js
+++ routes/users.js
@@ -4,10 +4,11 @@ const router = express.Router()
 const user = require('../controllers/user');
 
 router
-    .get('/',user.index)
-    .post('/',user.create)
-    .get('/:id',user.show)
-    .put('/:id',user.update)
-    .delete('/:id',user.destroy)
+    .get('/', user.index)
+    .get('/excel', user.downloadExcel)
+    .post('/', user.create)
+    .get('/:id', user.show)
+    .put('/:id', user.update)
+    .delete('/:id', user.destroy)
 
 module.exports = router
